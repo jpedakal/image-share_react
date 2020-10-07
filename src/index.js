4d301c88eb4164aa0577fe7b3265bcdd9b1ae568
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-10-07 16:53:53 +0530

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {BrowseRouter} from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   <React.StrictMode>
