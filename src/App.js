b2fbac6f89ea4461b1c444bfa03583fb0f8db2c1
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-10-10 16:39:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div className="App">
-      <h2>Welcome To Pro Shop</h2>
+      <Header />
+      <main>
+        <h2>Welcome To Pro Shop</h2>
+      </main>
+      <Footer />
     </div>
   );
 }
