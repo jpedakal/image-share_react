c0b83fe630bb96a99a3cec8b89beae249552b053
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-10-11 16:14:13 +0530

    Update Rating.js

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -23,6 +23,7 @@ const Rating = ({ value, text }) => {
                 <i className={value >= 5 ? 'fas fa-star' : value >= 4.5 ? 'fas fa-star-half-alt' : 'far fa-star'}>
                 </i>
             </span>
+            <span>{text && text}</span>
         </div>
     )
 }
