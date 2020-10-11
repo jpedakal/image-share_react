cd335221f98d70496564d9f988a610f61a09a3e6
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-10-11 15:44:04 +0530

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -18,9 +18,7 @@ const Product = ({ product }) => {
                         {product.rating} from {product.numReviews} reviews
                     </div>
                 </Card.Text>
-                <Card.Text as='h3'>
-                    {product.price}
-                </Card.Text>
+                <Card.Text as='h3'>${product.price}</Card.Text>
             </Card.Body>
         </Card>
     )
