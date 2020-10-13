13e9d3629ee85b0a7f9b145b007274441851c137
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-10-13 20:41:35 +0530

    Update ProductScreen.js

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
-import Rating from '../components/Rating'
+//import Rating from '../components/Rating'
 import products from '../products'
 
 
